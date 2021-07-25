edf8e286665f3db9fd38ecb54f6f4639dd356e77
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-25 17:57:35 +0700

    redirect to home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
+import { UserActionTypes } from './user.types';
+
 export const setCurrentUser = (user) => {
   console.log('setCurrentUser', user);
   return {
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
   };
 };
