7b46bcdf83ffe189e28d7f86f3e2244c6935005b
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-25 22:17:16 +0700

    implement increase and decrease quantity in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CartItem from '../../components/cart-item/cart-item.component';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {
@@ -32,7 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
 
     {cartItems.map((cartItem) => (
-      <CheckoutItem key={CartItem.id} cartItem={cartItem} />
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
 
     <div className='total'>
