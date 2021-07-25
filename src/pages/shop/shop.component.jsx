f4b2fe2b601990e9ac670dc6c1119aa0aa8b22de
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-26 00:03:36 +0700

    refactor data flow using hashmap instead of array

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectShopItems } from '../../redux/shop/shop.selectors';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
-      {collections.map(({ id, ...otherProps }) => (
-        <CollectionPreview key={id} {...otherProps} />
-      ))}
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopItems
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
