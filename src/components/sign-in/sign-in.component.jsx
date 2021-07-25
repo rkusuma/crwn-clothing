9bf0fc02289a83653a7ebe4d8216c918170b00e0
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-25 15:22:57 +0700

    fix button sign in with google triggering form submit

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -61,7 +61,10 @@ class SignIn extends React.Component {
 
           <div className='buttons'>
             <CustomButton type='submit'>Sign in</CustomButton>
-            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>
+            <CustomButton
+              type='button'
+              isGoogleSignIn
+              onClick={signInWithGoogle}>
               Sign in with Google
             </CustomButton>
           </div>
