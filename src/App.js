87981bd3f5dffe669279880cced63f9ef90fd047
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-25 15:17:35 +0700

    implemented firebase utils to sign up user

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -21,9 +21,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  handleSumit = async (event) => {
+    event.preventDefault();
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert("password don't match");
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    return (
+      <div className='sign-up'>
+        <h2 className='title'>I do not have a account</h2>
+        <span>Sign up with your email and password</span>
+        <form className='sign-up-form' onSubmit={this.handleSumit}>
+          <FormInput
+            type='text'
+            name='displayName'
+            label='Display Name'
+            required
+            value={displayName}
+            handleChange={this.handleChange}></FormInput>
+          <FormInput
+            type='text'
+            name='email'
+            label='Email'
+            required
+            value={email}
+            handleChange={this.handleChange}></FormInput>
+          <FormInput
+            type='password'
+            name='password'
+            label='Password'
+            required
+            value={password}
+            handleChange={this.handleChange}></FormInput>
+          <FormInput
+            type='password'
+            name='confirmPassword'
+            label='Confirm Password'
+            required
+            value={confirmPassword}
+            handleChange={this.handleChange}></FormInput>
+          <CustomButton type='submit'>Sign Up</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
