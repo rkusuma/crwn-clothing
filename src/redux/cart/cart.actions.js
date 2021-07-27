e95a6457a78fcf14bf07209ea44f1989e9a3602d
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-27 23:40:26 +0700

    add clear cart action and clear the cart when sign out success

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -26,3 +26,7 @@ export const clearItemFromCart = (item) => {
     payload: item
   };
 };
+
+export const clearCart = () => ({
+  type: CartActionTypes.CLEAR_CART
+});
--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { all, call, put, takeEvery } from 'redux-saga/effects';
 
 import {
   convertCollectionsSnapshotToMap,
@@ -31,4 +31,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
