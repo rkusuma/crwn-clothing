9ffc809a4e179302510be41981c9f3b9c1d0ac60
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-29 00:22:37 +0700

    add server express to handle payment, and move client code to client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100; // in cent
@@ -7,8 +8,24 @@ const StripeButton = ({ price }) => {
     'pk_test_51JHLnpIi30BTkcxx77oMk9fu55HOiSBFGJBopTObhDnU82lCN8btY5putblNr6vjaPhvt0gXWAlFjqNn8JbcBTXE00E9WHsTzC';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then((response) => {
+        console.log('stripe response', response);
+        alert('Payment Successful!');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
