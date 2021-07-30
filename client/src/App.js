88b8eee1ba82fc0786ad8cb9598d73e0a5fbb81a
Romy Kusuma <romy.kusuma57@gmail.com>
2021-07-30 22:56:55 +0700

    adjust mobile view styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,15 @@ import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-import Header from './components/header/header.component';
-
 import CheckoutPage from './pages/checkout/checkout.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { checkUserSession } from './redux/user/user.actions';
+import Header from './components/header/header.component';
 
+import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { GlobalStyle } from './global.styles';
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -21,6 +20,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
 
 export const SignInAndSignUpContainer = styled.div`
-  width: 800px;
+  width: 850px;
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+
+  @media screen and (max-width: 800px) {
+    width: unset;
+    flex-direction: column;
+    padding: 10px;
+    align-items: center;
+
+    > *:first-child {
+      margin-bottom: 50px;
+    }
+  }
 `;
